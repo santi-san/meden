91fae83e97ff1f658e86b2204553f7b24d86316c
Santi <it.santiagotoledo@gmail.com>
2021-07-12 22:17:36 -0300

    Home controller

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>index title page</h1>
+@stop
+
+@section('content')
+    <p>Here will be the content.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+@stop
