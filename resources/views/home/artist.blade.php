2d1f4bf46fb92f5a3fe348580f68206027de21f4
Santi <it.santiagotoledo@gmail.com>
2021-07-18 01:27:33 -0300

    working on artist layout

--- resources/views/home/artist.blade.php
+++ resources/views/home/artist.blade.php
@@ -9,19 +9,31 @@
 @section('content')
     <div class="row">
         {{-- Artist --}}
-        <div class="col-12 col-sm-12 col-lg-6 col-xl-12 pb-3">
+        <div class="col-12 col-sm-12 col-lg-12 col-xl-12 pb-3">
             <div class="bg-white rounded-lg py-3 px-3 " style="box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);">
-                <div class="d-block d-sm-flex" style="border-radius: .3rem;">
+                {{-- stadistics --}}
+                <div class="py-2 px-2 d-flex" style="background: #f4f6f9; position: absolute;top: 1rem; right:  24px;">
+                    <div role="button">
+                        <i class="fas fa-heart fa-xs"></i> 750
+                    </div >
+                    <div role="button" class="ml-4">
+                        <i class="fas fa-eye fa-xs"></i> 2
+                    </div>
+                </div>
+                {{-- Artist --}}
+                <div class="d-block d-lg-flex">
+                    {{-- Cover IMG --}}
                     <div style="max-height: 200px;width: 200px;margin-right: 16px;">
                         <img class="rounded-lg img-fluid" src="/storage/justice-band.jpg" alt="" style="height: 100%;object-fit: cover;width: 100%;">
                     </div>
-                    <div class="text-responsive text-truncate" style="width: calc(100% - 226px);">
-                        <h1>Justice </h1>
-                        <span class="text-sm text-truncate text-muted" style="display: block;">Member/s: Gaspard Auge / Xavier de Rosnay</span>
-                        <span class="text-sm text-truncate text-muted" style="display: block;">Origin: France</span>
-                        <span class="text-sm text-truncate text-muted" style="display: block;">Activity: 2003</span>
-
-                        <i class="fas fa-heart"></i> 750
+                    {{-- Data Artist --}}
+                    <div class="text-wrap" >
+                        <h1>Justice no mi in porti qua revort this</h1>
+                        <ul class="list-unstyled text-muted">
+                            <li><span class="font-weight-bold">Member/s:</span> Gaspard Auge - Xavier de Rosnay</span></li>
+                            <li><span class="font-weight-bold">Origin:</span> France</span></li>
+                            <li><span class="font-weight-bold">Since:</span> 2003</span></li>
+                        </ul>
                     </div>
                 </div>
             </div>
