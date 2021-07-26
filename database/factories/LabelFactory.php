f0a22ee651f6451a25d5b571a1941e58efc8421e
Santi <it.santiagotoledo@gmail.com>
2021-07-25 23:08:47 -0300

    make label -all + artist migration added founded_at

---
+++ database/factories/LabelFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Label;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LabelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Label::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
--- database/migrations/2021_07_26_020406_create_labels_table.php
+++ database/migrations/2021_07_26_020406_create_labels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArtistsTable extends Migration
+class CreateLabelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateArtistsTable extends Migration
      */
     public function up()
     {
-        Schema::create('artists', function (Blueprint $table) {
+        Schema::create('labels', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('members');
+            $table->string('origin');
             $table->date('foundation');
             $table->string('founded_at');
             $table->string('description');
@@ -33,6 +33,6 @@ class CreateArtistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('artists');
+        Schema::dropIfExists('labels');
     }
 }
